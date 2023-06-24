f72b639d2df6d8c65f6263690eae9529508be245
iammseyed <hey@you.com>
2023-06-24 19:24:11 +0530

    corrected indexNew.js

--- folder/indexNew.js
+++ folder/indexNew.js
@@ -1,7 +1,4 @@
 console.log("hello there");
 console.log("via newBranch from local machine side);
-<<<<<<< HEAD
 Iconsole.log("Adding another line without pulling from remote server");
-=======
 console.log("Via remote side");
->>>>>>> ee4dc1492ca6b7b8a20be6db4653a8a055ae3274
