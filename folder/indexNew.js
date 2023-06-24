04fecfad01a877cc0e1a6af0766c64c3662d0d6a
iammseyed <hey@you.com>
2023-06-24 19:38:02 +0530

    added again without pulling data

--- folder/indexNew.js
+++ folder/indexNew.js
@@ -3,3 +3,4 @@ console.log("via newBranch from local machine side);
 console.log("Adding another line without pulling from remote server");
 console.log("Via remote side");
 console.log("Hello again via git bash");
+console.log("Hello again without pulling data from remote side");
