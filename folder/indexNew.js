d0f8ed974837b8e1f6d5a22bc0e8034b4422aa90
iammseyed <hey@you.com>
2023-06-24 19:35:29 +0530

    added yet another line of code

--- folder/indexNew.js
+++ folder/indexNew.js
@@ -2,3 +2,4 @@ console.log("hello there");
 console.log("via newBranch from local machine side);
 console.log("Adding another line without pulling from remote server");
 console.log("Via remote side");
+console.log("Hello again via git bash");
