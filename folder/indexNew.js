c1e9821112c1b087acc79ff07daa55a38fcc433a
Seyed Muzafar <iamMseyed@users.noreply.github.com>
2023-06-24 19:24:56 +0530

    programitical mistake in indexNew.js

--- folder/indexNew.js
+++ folder/indexNew.js
@@ -1,4 +1,4 @@
 console.log("hello there");
 console.log("via newBranch from local machine side);
-Iconsole.log("Adding another line without pulling from remote server");
+console.log("Adding another line without pulling from remote server");
 console.log("Via remote side");
