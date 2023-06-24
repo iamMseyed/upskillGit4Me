cfee8bd632d9911b32dea2d3f3db22251fe53ca7
Seyed Muzafar <iamMseyed@users.noreply.github.com>
2023-06-24 16:27:45 +0530

    added package details to java file

--- folder/MainClass.java
+++ folder/MainClass.java
@@ -1,7 +1,7 @@
+package mainPackage;
 public MainClass{
 	public static void main(){
 		Sytem.out.println("In main method");
 		System.out.prinltn("Updated line via gitbash, will be pushed to github repo");
 	}
 }
-
